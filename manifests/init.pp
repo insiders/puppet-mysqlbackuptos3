fc7eeba9086c8352b574d7a4bf20c0419a806476
Martijn Heemels <martijn@insiders.nl>
2018-02-01 16:23:15 +0100

    Add test backup script. Requires AWS CLI tools.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,11 @@
 class mysqlbackuptos3 {
 
+  file { '/usr/local/sbin/mysqlbackuptos3.sh':
+    ensure  => present,
+    content => epp('mysqlbackuptos3/mysqlbackuptos3.sh.epp'),
+    mode    => '0700',
+    owner   => 'root',
+    group   => 'root',
+  }
+
 }
